b4d9a6efa1075526cc2d51f6cc4d81478bc2993f
Claudomiro Sales <claudomiro.sales@gmail.com>
2019-03-28 17:13:24 -0300

    Atividades com const static
    Criação 
    Inicialização no .h
    Inicialização no .cpp
    Uso no construtor
    Uso em um método no static
    Erros: usar sem inicializar e inicializar no construtor

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -1,11 +1,28 @@
 #include "Pessoa.h"
 
+//Pode ser inicializada no cpp
+const int Pessoa::nDigitosCPF = 13;
+
 Pessoa::Pessoa()
+//:nDigitosCPF(0) é da classe e não do objeto
 {
     cout << "Criada uma pessoa.\n";
+    
+    //ERROR - É const
+    //nDigitosCPF += 1;
+    
+    //No construtor
+    cout << "Construtor::nDigitosCPF " << nDigitosCPF << '\n';
+    
+    //Em um método no static
+    methodNoStatic( );
 }
 
 Pessoa::~Pessoa()
 {
 }
 
+void Pessoa::methodNoStatic( )
+{
+    cout << "MethodNoStatic::nDigitosCPF " << nDigitosCPF << '\n';
+}
--- Pessoa.h
+++ Pessoa.h
@@ -9,6 +9,21 @@ class Pessoa
 public:
     Pessoa();
     ~Pessoa();
+    
+private:
+    
+    //criada uma variável const static
+    const static int nDigitosCPF;
+    
+    //Pode inicializar no .h
+    //const static int nDigitosCPF = 13;
+    
+    //Pode ser inicializada no cpp
+    
+    //Mesmo sem inicialização não ocorre erro se não for usada em lugar algum
+    //Mas ocorre um erro se foi usada sem inicialização
+    
+    void methodNoStatic( );
 
 };
 
